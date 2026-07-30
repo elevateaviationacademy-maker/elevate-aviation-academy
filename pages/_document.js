d1fcf57e58322ae6d91ec7b77586c4dd7a69a44f
Elevate Aviation Academy <elevateaviationacademy@gmail.com>
2026-07-30 17:27:29 +0530

    Fix missing viewport meta tag causing inconsistent mobile zoom

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
         <meta name="theme-color" content="#0369a1" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
